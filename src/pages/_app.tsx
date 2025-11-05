4f72b24afec6b9648fc1e904eecffeec6cb2da44
Vladislav15-maker <vladigra121@email.com>
2025-11-05 09:58:00 +0500

    fix: configure paths for src directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,2 +1,13 @@
-import { AppProvider } from 'context/AppContext'; // или '../context/AppContext' если ваш _app.tsx в pages
-import 'styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AppProvider } from '@/context/AppContext'; // <-- Используем новый псевдоним @
+import '@/styles/globals.css';                 // <-- Используем новый псевдоним @
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
+}
+
+export default MyApp;
